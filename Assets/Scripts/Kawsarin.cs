54167ac063d1dedef3d3f0747386facc0d8a513b
Piero Nole <pieronole1317@gmail.com>
2025-09-06 21:31:48 -0500

    Agregando sprites al personaje y correccion del movimiento horizontal

--- Assets/Scripts/Kawsarin.cs
+++ Assets/Scripts/Kawsarin.cs
@@ -3,12 +3,13 @@ using UnityEngine.InputSystem;
 
 public class Kawsarin : MonoBehaviour
 {
-    public float velocidad = 4f;
-
+    public float velocidad = 5f;
+    private Rigidbody2D rb;
+    private bool miraDerecha = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -19,13 +20,19 @@ public class Kawsarin : MonoBehaviour
     void MoviminetoPersonaje()
     {
         float inputMovimiento = 0f;
-        Rigidbody2D rb = GetComponent<Rigidbody2D>();
-        if (Keyboard.current.aKey.isPressed) inputMovimiento = -1f;
-        if (Keyboard.current.dKey.isPressed) inputMovimiento = 1f;
-
+        if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed) inputMovimiento = -1f;
+        if (Keyboard.current.dKey.isPressed|| Keyboard.current.rightArrowKey.isPressed) inputMovimiento = 1f;
         rb.linearVelocity = new Vector2(inputMovimiento * velocidad, rb.linearVelocity.y);
+        GestionarOrientacion(inputMovimiento);  
+    }
+    void GestionarOrientacion(float inputMovimiento)
+    {
+        if (miraDerecha == true && inputMovimiento < 0 || miraDerecha == false && inputMovimiento > 0)
+        {
+            miraDerecha = !miraDerecha;
+            transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
 
-
+        }
     }
 
 }

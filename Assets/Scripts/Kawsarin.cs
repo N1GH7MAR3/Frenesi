0c2a49c55b78c91546a2a4efbf930e5cccef98bf
Piero Nole <pieronole1317@gmail.com>
2025-09-07 21:49:17 -0500

    Corrigiendo salto, agregando entorno y fisicas

--- Assets/Scripts/Kawsarin.cs
+++ Assets/Scripts/Kawsarin.cs
@@ -3,28 +3,35 @@ using UnityEngine.InputSystem;
 
 public class Kawsarin : MonoBehaviour
 {
-    public float velocidad = 5f;
+    public float fuerzaSalto = 15f;
+    public float velocidad = 8f;
     private Rigidbody2D rb;
+    private CapsuleCollider2D bc;
+    public LayerMask suelo;
     private bool miraDerecha = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        bc = GetComponent<CapsuleCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         MoviminetoPersonaje();
+        Salto();
     }
+
     void MoviminetoPersonaje()
     {
         float inputMovimiento = 0f;
         if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed) inputMovimiento = -1f;
-        if (Keyboard.current.dKey.isPressed|| Keyboard.current.rightArrowKey.isPressed) inputMovimiento = 1f;
+        if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed) inputMovimiento = 1f;
         rb.linearVelocity = new Vector2(inputMovimiento * velocidad, rb.linearVelocity.y);
-        GestionarOrientacion(inputMovimiento);  
+        GestionarOrientacion(inputMovimiento);
     }
+
     void GestionarOrientacion(float inputMovimiento)
     {
         if (miraDerecha == true && inputMovimiento < 0 || miraDerecha == false && inputMovimiento > 0)
@@ -33,6 +40,19 @@ public class Kawsarin : MonoBehaviour
             transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
 
         }
+    }
+    bool EstaEnSuelo()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(bc.bounds.center, new Vector2(bc.bounds.size.x, bc.bounds.size.y), 0f, Vector2.down, 0.9f, suelo);
+        return raycastHit.collider!=null;
+    }
+    void Salto()
+    {
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && EstaEnSuelo())
+        {
+            rb.AddForce(Vector2.up * fuerzaSalto,ForceMode2D.Impulse);
+
+         }
     }
 
 }
